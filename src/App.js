3acbdf84ba7f1736e1bd0f2f4649da55405753ea
rishi <rishisingh0831@gmail.com>
2019-12-15 12:07:33 +0530

    added newgame button, reset score button, scores at top, buttons are disabled when clicked ones

--- src/App.js
+++ src/App.js
@@ -18,6 +18,21 @@ import {
 import { FaLinkedin, FaGithubSquare, FaCodepen } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
+var btn1Classes = "btn1";
+var btn2Classes = "btn2";
+var btn3Classes = "btn3";
+var btn4Classes = "btn4";
+var btn5Classes = "btn5";
+var btn6Classes = "btn6";
+var btn7Classes = "btn7";
+var btn8Classes = "btn8";
+var btn9Classes = "btn9";
+
+function returnLastClass(classes) {
+  var classesArray = classes.split(" ");
+  return classesArray.pop();
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -35,22 +50,29 @@ class App extends Component {
       draw: false, //this checks if the match is draw or not
       whiteScore: 0, //keeps cunt of the score of white
       blackScore: 0, //keeps track od black score
-      memoryArray: ["a", "b", "c", "d", "e", "f", "g", "h", "i"] //keeps track of which elements have been
+      memoryArray: [] //keeps track of which elements have been
     };
     this.play = this.play.bind(this);
-    this.resetButtonComponent = this.resetButtonComponent.bind(this);
+    this.reset = this.reset.bind(this);
+    this.newGame = this.newGame.bind(this);
   }
 
   toggleNavbar = () => this.setState({ collapsed: !this.state.collapsed });
 
   play(btn) {
+    var resBox = document.getElementById("res");
+    // resbox is the div whisch displays the result after each game.
     if (this.state.play == 0) {
+      // play turns to 1 when there is a winner and when we start a new game, play turns to 0
       if (this.state.player == 0) {
+        // this checks which player is playing 0 means white and 1 means black
         this.setState({ player: 1 });
         if (this.state.count < 9) {
+          // this checks if all 9 boxes are filled, if all boxes are not filled then game will continue, if all boxes are filled then we check if there is a winner or not, if there is no winner then mach is declayered draw
           console.log("if statement 11");
           this.setState({ count: this.state.count + 1 });
           if (!this.state.winner) {
+            // this checks if a playes has won the game if not the game will continue
             console.log("if statement 21");
             var arr = this.state.dataArray;
             if (
@@ -63,6 +85,7 @@ class App extends Component {
               (arr[0][0] === arr[1][1] && arr[0][0] === arr[2][2]) ||
               (arr[0][2] === arr[1][1] && arr[0][2] === arr[2][0])
             ) {
+              // this is how we check for the winner, there are 8 ways of winning the game, all 8 are verified above. if there is no winner we go to the else statement whre the game continues
               console.log("white wins");
               this.setState({
                 winner: true,
@@ -70,53 +93,63 @@ class App extends Component {
               });
               this.setState({ play: 1 });
             } else {
+              // here classes are addded too the buttons and valued are inserted in the data araay
               console.log("else statement 18");
               var value = btn.target.value;
               if (value == 1) {
+                // btn 1
                 arr[0][0] = 0;
                 console.log(arr);
-                var element1 = document.querySelector(".btn1");
-                element1.classList.add("btn-light");
+                btn1Classes = "btn1 btn-light";
+                document.getElementById("1").setAttribute("disabled", true);
               } else if (value == 2) {
+                // btn 2
                 arr[0][1] = 0;
                 console.log(arr);
-                var element2 = document.querySelector(".btn2");
-                element2.classList.add("btn-light");
+                btn2Classes = "btn2 btn-light";
+                document.getElementById("2").setAttribute("disabled", true);
               } else if (value == 3) {
+                // btn 3
                 arr[0][2] = 0;
                 console.log(arr);
-                var element3 = document.querySelector(".btn3");
-                element3.classList.add("btn-light");
+                btn3Classes = "btn3 btn-light";
+                document.getElementById("3").setAttribute("disabled", true);
               } else if (value == 4) {
+                // btn 4
                 arr[1][0] = 0;
                 console.log(arr);
-                var element4 = document.querySelector(".btn4");
-                element4.classList.add("btn-light");
+                btn4Classes = "btn4 btn-light";
+                document.getElementById("4").setAttribute("disabled", true);
               } else if (value == 5) {
+                // btn 5
                 arr[1][1] = 0;
                 console.log(arr);
-                var element5 = document.querySelector(".btn5");
-                element5.classList.add("btn-light");
+                btn5Classes = "btn5 btn-light";
+                document.getElementById("5").setAttribute("disabled", true);
               } else if (value == 6) {
+                // btn 6
                 arr[1][2] = 0;
                 console.log(arr);
-                var element6 = document.querySelector(".btn6");
-                element6.classList.add("btn-light");
+                btn6Classes = "btn6 btn-light";
+                document.getElementById("6").setAttribute("disabled", true);
               } else if (value == 7) {
+                // btn 7
                 arr[2][0] = 0;
                 console.log(arr);
-                var element7 = document.querySelector(".btn7");
-                element7.classList.add("btn-light");
+                btn7Classes = "btn7 btn-light";
+                document.getElementById("7").setAttribute("disabled", true);
               } else if (value == 8) {
+                // btn 8
                 arr[2][1] = 0;
                 console.log(arr);
-                var element8 = document.querySelector(".btn8");
-                element8.classList.add("btn-light");
+                btn8Classes = "btn8 btn-light";
+                document.getElementById("8").setAttribute("disabled", true);
               } else if (value == 9) {
+                // btn 9
                 arr[2][2] = 0;
                 console.log(arr);
-                var element9 = document.querySelector(".btn9");
-                element9.classList.add("btn-light");
+                btn9Classes = "btn9 btn-light";
+                document.getElementById("9").setAttribute("disabled", true);
               } else {
                 console.log("not working");
               }
@@ -130,18 +163,18 @@ class App extends Component {
                 (arr[0][0] === arr[1][1] && arr[0][0] === arr[2][2]) ||
                 (arr[0][2] === arr[1][1] && arr[0][2] === arr[2][0])
               ) {
+                // we check for the winner again because dataArray has been updated in the else statement above.
                 console.log("white wins");
                 this.setState({
                   winner: true,
                   whiteScore: this.state.whiteScore + 1
                 });
                 this.setState({ play: 1 });
-                var resBox = document.getElementById("res");
                 resBox.innerHTML = "<h3> White Wins</h3>";
               } else if (this.state.count > 7) {
+                // this checks for match draw conditions. If the count is 1 less then 8(ie. 1 box is not filled rest all are filled)(count starts from 0) then we say the match os draw
                 console.log("Match Draew");
                 this.setState({ draw: true });
-                var resBox = document.getElementById("res");
                 resBox.innerHTML = "<h3> Match Draw</h3>";
               }
             }
@@ -150,7 +183,6 @@ class App extends Component {
           this.setState({ play: 1 });
           console.log("count up ie 9 in black draw match");
           this.setState({ draw: true });
-          var resBox = document.getElementById("res");
           resBox.innerHTML = "<h3> Match Draw</h3>";
         }
       } else if (this.state.player == 1) {
@@ -183,48 +215,48 @@ class App extends Component {
               if (value == 1) {
                 arr[0][0] = 1;
                 console.log(arr);
-                var element1 = document.querySelector(".btn1");
-                element1.classList.add("btn-dark");
+                btn1Classes = "btn1 btn-dark";
+                document.getElementById("1").setAttribute("disabled", true);
               } else if (value == 2) {
                 arr[0][1] = 1;
                 console.log(arr);
-                var element2 = document.querySelector(".btn2");
-                element2.classList.add("btn-dark");
+                btn2Classes = "btn2 btn-dark";
+                document.getElementById("2").setAttribute("disabled", true);
               } else if (value == 3) {
                 arr[0][2] = 1;
                 console.log(arr);
-                var element3 = document.querySelector(".btn3");
-                element3.classList.add("btn-dark");
+                btn3Classes = "btn3 btn-dark";
+                document.getElementById("3").setAttribute("disabled", true);
               } else if (value == 4) {
                 arr[1][0] = 1;
                 console.log(arr);
-                var element4 = document.querySelector(".btn4");
-                element4.classList.add("btn-dark");
+                btn4Classes = "btn4 btn-dark";
+                document.getElementById("4").setAttribute("disabled", true);
               } else if (value == 5) {
                 arr[1][1] = 1;
                 console.log(arr);
-                var element5 = document.querySelector(".btn5");
-                element5.classList.add("btn-dark");
+                btn5Classes = "btn5 btn-dark";
+                document.getElementById("5").setAttribute("disabled", true);
               } else if (value == 6) {
                 arr[1][2] = 1;
                 console.log(arr);
-                var element6 = document.querySelector(".btn6");
-                element6.classList.add("btn-dark");
+                btn6Classes = "btn6 btn-dark";
+                document.getElementById("6").setAttribute("disabled", true);
               } else if (value == 7) {
                 arr[2][0] = 1;
                 console.log(arr);
-                var element7 = document.querySelector(".btn7");
-                element7.classList.add("btn-dark");
+                btn7Classes = "btn7 btn-dark";
+                document.getElementById("7").setAttribute("disabled", true);
               } else if (value == 8) {
                 arr[2][1] = 1;
                 console.log(arr);
-                var element8 = document.querySelector(".btn8");
-                element8.classList.add("btn-dark");
+                btn8Classes = "btn8 btn-dark";
+                document.getElementById("8").setAttribute("disabled", true);
               } else if (value == 9) {
                 arr[2][2] = 1;
                 console.log(arr);
-                var element9 = document.querySelector(".btn9");
-                element9.classList.add("btn-dark");
+                btn9Classes = "btn9 btn-dark";
+                document.getElementById("9").setAttribute("disabled", true);
               } else {
                 console.log("not working");
               }
@@ -239,7 +271,6 @@ class App extends Component {
                 (arr[0][2] === arr[1][1] && arr[0][2] === arr[2][0])
               ) {
                 console.log("black wins");
-                var resBox = document.getElementById("res");
                 resBox.innerHTML = "<h3> Black Wins</h3>";
                 this.setState({
                   winner: true,
@@ -249,7 +280,6 @@ class App extends Component {
               } else if (this.state.count > 7) {
                 console.log("Match Draew");
                 this.setState({ draw: true });
-                var resBox = document.getElementById("res");
                 resBox.innerHTML = "<h3> Match Draw</h3>";
               }
             }
@@ -258,24 +288,53 @@ class App extends Component {
           this.setState({ play: 1 });
           console.log("count up ie 9 in black, match draw");
           this.setState({ draw: true });
-          var resBox = document.getElementById("res");
           resBox.innerHTML = "<h3> Match Draw </h3>";
         }
       }
     }
   }
 
-  resetButtonComponent = () => {
-    if (this.state.winner || this.state.draw) {
-      return (
-        <Button className="btn btn-primary" onClick={this.reset}>
-          Reset
-        </Button>
-      );
-    }
+  newGame = () => {
+    this.setState({
+      count: 0,
+      winner: false,
+      dataArray: [
+        ["a", "b", "c"],
+        ["d", "e", "f"],
+        ["g", "h", "i"]
+      ],
+      player: 0,
+      play: 0,
+      draw: false,
+      memoryArray: []
+    });
+    console.log("new game btn");
+    btn1Classes = "btn1";
+    btn2Classes = "btn2";
+    btn3Classes = "btn3";
+    btn4Classes = "btn4";
+    btn5Classes = "btn5";
+    btn6Classes = "btn6";
+    btn7Classes = "btn7";
+    btn8Classes = "btn8";
+    btn9Classes = "btn9";
+    document.getElementById("1").removeAttribute("disabled");
+    document.getElementById("2").removeAttribute("disabled");
+    document.getElementById("3").removeAttribute("disabled");
+    document.getElementById("4").removeAttribute("disabled");
+    document.getElementById("5").removeAttribute("disabled");
+    document.getElementById("6").removeAttribute("disabled");
+    document.getElementById("7").removeAttribute("disabled");
+    document.getElementById("8").removeAttribute("disabled");
+    document.getElementById("9").removeAttribute("disabled");
+    var resBox = document.getElementById("res");
+    resBox.textContent = "";
   };
 
-  reset = () => {};
+  reset = () => {
+    this.setState({ whiteScore: 0, blackScore: 0 });
+    console.log(this.state.whiteScore, this.state.blackScore, "reset game btn");
+  };
 
   render() {
     return (
@@ -326,42 +385,85 @@ class App extends Component {
             </NavbarText>
           </Collapse>
         </Navbar>
-        <div className="game col-10 col-xs-4 col-sm-5 col-lg-4 offset-1 offset-xs-3 offset-sm-3 offset-lg-4 p-5 mt-5">
+        <div className="col-8 offset-2 mt-4" id="score">
           <div>
-            <Button className="btn1" onClick={this.play} value={1}></Button>
-            <Button className="btn2" onClick={this.play} value={2}></Button>
-            <Button className="btn3" onClick={this.play} value={3}></Button>
-          </div>
-          <div>
-            <Button className="btn4" onClick={this.play} value={4}></Button>
-            <Button className="btn5" onClick={this.play} value={5}></Button>
-            <Button className="btn6" onClick={this.play} value={6}></Button>
-          </div>
-          <div>
-            <Button className="btn7" onClick={this.play} value={7}></Button>
-            <Button className="btn8" onClick={this.play} value={8}></Button>
-            <Button className="btn9" onClick={this.play} value={9}></Button>
+            <h3>White Score: {this.state.whiteScore}</h3>
+            <h3>Black Score: {this.state.blackScore}</h3>
           </div>
         </div>
-        {/* <div className="contactDetails">
-          <h1>
-            <a
-              href="https://www.linkedin.com/in/rishi-singh-b2226415b/"
-              className="link"
-            >
-              <FaLinkedin />
-            </a>
-            <a href="https://github.com/rishi-singh26" className="link">
-              <FaGithubSquare />
-            </a>
-            <a href="https://codepen.io/rishisingh-26/" className="link">
-              <FaCodepen />
-            </a>
-          </h1>
-        </div> */}
+        <br />
+        <div className="game col-10 col-xs-4 col-sm-5 col-lg-4 offset-1 offset-xs-3 offset-sm-3 offset-lg-4 p-5 mt-2">
+          <div>
+            <Button
+              className={btn1Classes}
+              onClick={this.play}
+              value={1}
+              id={1}
+            ></Button>
+            <Button
+              className={btn2Classes}
+              onClick={this.play}
+              value={2}
+              id={2}
+            ></Button>
+            <Button
+              className={btn3Classes}
+              onClick={this.play}
+              value={3}
+              id={3}
+            ></Button>
+          </div>
+          <div>
+            <Button
+              className={btn4Classes}
+              onClick={this.play}
+              value={4}
+              id={4}
+            ></Button>
+            <Button
+              className={btn5Classes}
+              onClick={this.play}
+              value={5}
+              id={5}
+            ></Button>
+            <Button
+              className={btn6Classes}
+              onClick={this.play}
+              value={6}
+              id={6}
+            ></Button>
+          </div>
+          <div>
+            <Button
+              className={btn7Classes}
+              onClick={this.play}
+              value={7}
+              id={7}
+            ></Button>
+            <Button
+              className={btn8Classes}
+              onClick={this.play}
+              value={8}
+              id={8}
+            ></Button>
+            <Button
+              className={btn9Classes}
+              onClick={this.play}
+              value={9}
+              id={9}
+            ></Button>
+          </div>
+        </div>
+        <div>
+          <Button className="newGameBtn btn-warning" onClick={this.newGame}>
+            New Game
+          </Button>
+          <Button className="resetScoreBtn btn-danger" onClick={this.reset}>
+            Reset Scores
+          </Button>
+        </div>
         <br />
         <div className="col-8 offset-2" id="res"></div>
-        <div>{this.resetButtonComponent}</div>
       </div>
     );
   }
